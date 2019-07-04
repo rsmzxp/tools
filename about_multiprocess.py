2f6d96b32e3a07099702589dd6404b1ed281e647
rsmzxp <33628310+rsmzxp@users.noreply.github.com>
2019-07-04 16:40:47 +0800

    Update about_multiprocess.py

--- about_multiprocess.py
+++ about_multiprocess.py
@@ -35,3 +35,20 @@ if __name__ == '__main__':
     a_b=zip(a,b)
     pool.map(multi_wrapper,list(a_b))
     pool.close()
+
+&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+def my_func(x,y):
+    print(x*y)
+
+
+if __name__ == '__main__':
+    # n是进程数
+    pool = Pool(5)
+    # names是一个list,my_dir是一个相对固定参数
+    a = [x for x in range(100)]
+    b = [x for x in range(100,0,-1)]
+    # z = 10
+    a_b=zip(a,b)
+    pool.starmap(my_func,list(a_b))
+    pool.close()
+    pool.join()
